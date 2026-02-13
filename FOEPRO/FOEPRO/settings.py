65e330ddb41ea9c32ddc4c83edd6af12289f9d36
Kush <kushvivek.sandam017@nmims.in>
2026-02-13 18:42:19 +0530

    Archives-3
    Fixed many errors

--- FOEPRO/FOEPRO/settings.py
+++ FOEPRO/FOEPRO/settings.py
@@ -198,10 +198,8 @@ LOGGING = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'Archives' / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'Archives' / 'static'
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
