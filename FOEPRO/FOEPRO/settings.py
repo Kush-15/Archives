cf94595c01db5d4683c61e3f6b97f298c164db28
Kush <kushvivek.sandam017@nmims.in>
2026-02-13 18:20:12 +0530

    Update settings.py

--- FOEPRO/FOEPRO/settings.py
+++ FOEPRO/FOEPRO/settings.py
@@ -110,20 +110,30 @@ WSGI_APPLICATION = 'FOEPRO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'CONN_MAX_AGE': int(os.getenv('CONN_MAX_AGE', 0)),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+DB_NAME = os.getenv('DB_NAME')
+
+if DB_NAME:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': DB_NAME,
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'CONN_MAX_AGE': int(os.getenv('CONN_MAX_AGE', 0)),
+            'OPTIONS': {
+                'sslmode': 'require',
+            },
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
